3f733c1e456482ee38f7ac02584f4833d5eef885
Qiu Jianlin <jianlin.qiu@intel.com>
2019-03-05 03:15:03 +0800

    remvoe obsolete

--- installConfig.sh
+++ installConfig.sh
@@ -8,10 +8,6 @@ echo -e "source $CURRENT_DIR/vimrc" >> ~/.vimrc
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +PluginInstall +qall
 
-#alias vimtags='(echo "!_TAG_FILE_SORTED    2   /2=foldcase/"; \
-#	(find . \( -name .svn -o -name .repo -o -name .git \) -prune -o -type f -printf "%f\t%p\t1\n" | \
-#	sort -f)) > ./filenametags; gtags -i'
-
 # git
 echo -e "[include]\n\tpath = $CURRENT_DIR/gitconfig" >> ~/.gitconfig
 
