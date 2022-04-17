62fa6002865fa754fcb5e7d8837cc59fde810bf9
Jianhui Dai <jianhui.j.dai@intel.com>
2022-04-17 08:01:00 +0800

    add install vim

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-THIS=`pwd`/$0
+THIS=$(realpath $0)
 if [ ! -f ${THIS} ]; then
-    THIS=$0
+  THIS=$0
 fi
-ROOT=`dirname ${THIS}`
+
+ROOT=$(dirname ${THIS})
 
 source /etc/os-release
 if [ $NAME = "Ubuntu" ]; then
@@ -16,15 +16,10 @@ samba cifs-utils build-essential automake global cmake
     sudo -E apt install -y fortunes fortunes-zh cowsay lolcat
 fi
 
-# vim
-echo -e "source ${ROOT}/vimrc" >> ~/.vimrc
-
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +PluginInstall +qall
-
 # git
 echo -e "[include]\n\tpath = ${ROOT}/gitconfig" >> ~/.gitconfig
 
 # tmux
 ln -s -v ${ROOT}/tmux.conf ~/.tmux.conf
 
+${ROOT}/install_vim.sh
---
+++ install_vim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+THIS=`pwd`/$0
+if [ ! -f ${THIS} ]; then
+    THIS=$0
+fi
+ROOT=`dirname ${THIS}`
+
+# vim
+echo -e "source ${ROOT}/vimrc" >> ~/.vimrc
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +PluginInstall +qall
+
