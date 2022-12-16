bf7f153f2a17a073dcc37db34041681f0f326012
Jianhui Dai <jianhui.j.dai@intel.com>
2022-12-16 08:43:25 +0800

    Add test webrtc

--- webrtc/googcc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-OUTPUT=gcc.log
-
-./src/out/Default/modules_unittests --gtest_filter="*GoogCc*" |&
-	tee $OUTPUT
-
-echo "===Done==="
-grep -E "PASSED|FAILED" $OUTPUT
--- webrtc/peerconnection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-OUTPUT=gcc.log
-
-./src/out/Default/peerconnection_unittests |&
-	tee $OUTPUT
-
-echo "===Done==="
-grep -E "PASSED|FAILED" $OUTPUT
