683469e35a4de0bb7d47534ad3bbe21b6e12e915
Jianhui Dai <jianhui.j.dai@intel.com>
2023-03-28 21:25:32 +0800

    Add vpp ivf stat

--- chromium/tests/video_accelerator_tests/run_vea_tests.sh
+++ chromium/tests/video_accelerator_tests/run_vea_tests.sh
@@ -6,16 +6,20 @@ if [ ! -f ${THIS} ]; then
 fi
 CURRENT=$(dirname ${THIS})
 
-PREFIX=$(realpath ${CURRENT}/../src/out/Default/)
+#PREFIX=$(realpath ${CURRENT}/../src/out/Default/)
+PREFIX=${CURRENT}/../src/out/Default/
 
-data=${CURRENT}/data/staitc-screen-1280x720-300frames.i420.yuv
+echo "${PREFIX}"
+
+#data=${CURRENT}/data/staitc-screen-1280x720-300frames.i420.yuv
 #data=${CURRENT}/data/staitc-screen-640x360-300frames.i420.yuv
-#data=${CURRENT}/data/staitc-screen-320x180-300frames.i420.yuv
+data=${CURRENT}/data/staitc-screen-320x180-300frames.i420.yuv
 
 rm VideoEncoderTest -rf
 
 ${PREFIX}/video_encode_accelerator_tests \
   --codec=vp9 \
+  --bitrate=288000\
   --output_bitstream ${data} \
   --gtest_filter=VideoEncoderTest.FlushAtEndOfStream \
   --vmodule=*/media/gpu/*=4 |& tee vea_tests.log
