2a32f33b95db34d535619ddfda4116819a62a5ad
Jianhui Dai <jianhui.j.dai@intel.com>
2023-05-07 17:02:05 +0800

    Set bitrate

--- chromium/tests/vea_test/run_vea_tests.sh
+++ chromium/tests/vea_test/run_vea_tests.sh
@@ -8,12 +8,32 @@ CURRENT=$(dirname ${THIS})
 
 PREFIX=~/third_party/nfs/chromium-linux/src/out/Default/
 
-CODEC="vp9"
-BITRATE=2250000
 LOG=vea_tests.log
 
-INPUT_DIR=~/third_party/test_streams/vp9_lossless_static
-INPUT_YUV=youtube_screenshot_static.webm
+#CODEC="vp9"
+#BITRATE=2250000
+#INPUT_DIR=~/third_party/test_streams/vp9_lossless_static
+#INPUT_YUV=youtube_screenshot_static.webm
+
+# pc_full_stack_tests.cc - Chromium Code Search
+# https://source.chromium.org/chromium/chromium/src/+/main:third_party/webrtc/video/pc_full_stack_tests.cc;l=1268?q=1500&ss=chromium%2Fchromium%2Fsrc:third_party%2Fwebrtc%2F
+
+# 720p
+CODEC="vp9"
+BITRATE=1500000
+INPUT_DIR=~/Videos/gipsrestat
+INPUT_YUV=gipsrestat-1280x720_20210211.vp9.webm
+# 360
+CODEC="vp9"
+BITRATE=500000
+INPUT_DIR=~/Videos/gipsrestat
+INPUT_YUV=gipsrestat-640x360_20210211.vp9.webm
+# 180
+CODEC="vp9"
+BITRATE=150000
+INPUT_DIR=~/Videos/gipsrestat
+INPUT_YUV=gipsrestat-320x180_20210211.vp9.webm
+
 INPUT=${INPUT_DIR}/${INPUT_YUV}
 
 rm VideoEncoderTest -rf
