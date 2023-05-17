4d9f9e38e22195c518f19597656d440e4904d1fc
Jianhui Dai <jianhui.j.dai@intel.com>
2023-05-17 16:45:44 +0800

    up

--- chromium/deploy_to_cros.sh
+++ chromium/deploy_to_cros.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -ex
 
-#OUT=out_amd64-generic/Release
-OUT=out_amd64-generic/base-build
-#OUT=out_amd64-generic/opt-event-log+p2p-build
+OUT=out_amd64-generic/Release
 IP_ADDR=10.239.158.125
 
 deploy_chrome --build-dir=${OUT} --device=$IP_ADDR
