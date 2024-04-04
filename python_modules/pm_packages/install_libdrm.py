a36704ccf9e0b4dceb80514e83b1435a1ef13141
Jianhui Dai <jianhui.j.dai@intel.com>
2024-04-04 10:12:56 +0800

    Add libdrm build helper

---
+++ python_modules/pm_packages/install_libdrm.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3 -u
+
+import sys
+import os
+import re
+import pathlib
+import subprocess
+import argparse
+import shutil
+
+# local modules
+import pm_shell
+import pm_packages
+from pm_shell import run_shell as run_shell
+
+
+def install_deps():
+    os_release = None
+    with open('/etc/os-release', encoding="utf-8") as f:
+        os_release = f.readline()
+
+    if os_release and re.search('Ubuntu', os_release):
+        print(f'OS Ubuntu')
+        '''
+        packages = ''
+        packages += f'build-essential cmake libtool wget tcl '
+
+        cmd = f'sudo -E apt install -y ' + packages
+        run_shell(cmd)
+        '''
+    else:
+        raise RuntimeError(f'Unsupported OS {os_release}')
+
+
+def install_libaom(source_dir, prefix):
+    name = 'drm'
+    url = f'https://gitlab.freedesktop.org/mesa/drm.git'
+    version = ''
+    configure = f''
+
+    dst = source_dir.joinpath(name)
+    pm_packages.clone_git_repo(url, version, dst, None)
+
+    cwd = os.getcwd()
+    os.chdir(str(dst))
+
+    builddir = 'builddir'
+    cmd = f'meson {builddir}'
+    run_shell(cmd)
+
+    cmd = f'meson configure {builddir} -Dprefix={prefix} -Dinstall-test-programs=true'
+    run_shell(cmd)
+
+    cmd = f'ninja -C {builddir} install'
+    run_shell(cmd)
+
+    os.chdir(cwd)
+    return
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument('-r',
+                        '--root',
+                        dest='root',
+                        action='store',
+                        default=None,
+                        help="Set root install path")
+    parser.add_argument('-d',
+                        '--deps',
+                        dest='install_deps',
+                        action='store_true',
+                        default=False,
+                        help="Install all deps")
+    parser.add_argument('-g',
+                        '--git',
+                        dest='use_git',
+                        action='store_true',
+                        default=False,
+                        help="Use git repo")
+    args = parser.parse_args()
+
+    # set pwd
+    pwd = pathlib.Path().resolve()
+    if args.root:
+        pwd = pathlib.Path(args.root).resolve()
+    print(f'Set PWD, {pwd}')
+
+    source_dir = pwd.joinpath('third_party')
+    prefix = pwd.joinpath('out')
+
+    # install deps
+    if args.install_deps:
+        install_deps()
+
+    install_libaom(source_dir, prefix)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
