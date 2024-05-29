a5dad0aa833c0a94a226d47f13539fddd49883c3
Jianhui Dai <jianhui.j.dai@intel.com>
2024-05-29 22:51:03 +0800

    [tests] run tflite benchmark

--- chromium/run_chrome_linux.sh
+++ chromium/run_chrome_linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ##!/bin/bash -ex
 
+## using google-chrome
+USING_GOOGLE_CHROME=false
+
 ## define
 URL=$1
 Y4M_FILE="~/Videos/bbb_1280x720-100frames.y4m"
@@ -34,10 +37,26 @@ if [ ${USE_GDB} == "true" ]; then
 --debug --single-process"
 fi
 
+CHROME_EXE="${PREFIX}/chrome"
+if [ ${USING_GOOGLE_CHROME} == "true" ]; then
+    CHROME_EXE="google-chrome"
+fi
+
+# mesa dump
+export MESA_GLSL=source,log,useprog
+export MESA_SHADER_DUMP_PATH=/tmp/MESA_SHADER_DUMP_PATH
+export MESA_SHADER_CAPTURE_PATH=/tmp/MESA_SHADER_CAPTURE_PATH
+
+mkdir -p ${MESA_SHADER_DUMP_PATH}
+mkdir -p ${MESA_SHADER_CAPTURE_PATH}
+
+mkdir -p /tmp/angle_shaders
+export ANGLE_SHADER_DUMP_PATH=/tmp/angle_shaders
+
 ## run
 CMD="\
 ${GDB_CMD} \
-${PREFIX}/chrome \
+${CHROME_EXE} \
 --ignore-gpu-blocklist \
 --disable-gpu-driver-bug-workaround \
 \
@@ -51,6 +70,11 @@ ${PREFIX}/chrome \
 \
 --vmodule=*/ozone/*=1,*/wayland/*=1,*/vaapi/*=1,*/viz/*=1,*/media/*=1,*/shared_image/*=1 \
 --enable-logging=stderr --v=0 \
+\
+--disable-gpu-sandbox \
+--enable-angle-features=dumpShaderSource \
+--enable-angle-features=enableShaderSubstitution \
+--enable-angle-features=dumpShaderSource,enableShaderSubstitution \
 \
 ${EXTRA_OPTIONS} \
 ${URL}"
